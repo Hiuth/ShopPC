cc78c3b71d522e6e7fcbbffea3ed45ff9b14c695
Hiuth <Hiuth2004@gmail.com>
2025-10-13 10:53:09 +0700

    Add CartRepository and ICartRepository for cart operations
    Introduced `CartRepository` to implement cart-specific operations
    and `ICartRepository` as its interface. Added methods to retrieve
    carts by user ID, clear a specific cart, and clear all carts for
    an account. Updated necessary `using` directives to include
    required namespaces.

---
+++ Repository/ImplementationsRepository/CartRepository.cs
@@ -0,0 +1,34 @@
+﻿using ShopPC.Data;
+using Microsoft.EntityFrameworkCore;
+using ShopPC.Repository.InterfaceRepository;
+using System.Linq.Expressions;
+using ShopPC.Models;
+
+namespace ShopPC.Repository.ImplementationsRepository
+{
+    public class CartRepository: GenericReopository<Cart>,ICartRepository
+    {
+        public CartRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Cart>> GetCartsByUserIdAsync(string userId)
+        {
+            return await _dbSet.Where(c => c.accountId == userId).ToListAsync();
+        }
+
+        public async Task ClearCartAsync(string cartId)
+        {
+            var cartItems = await _dbSet.Where(c => c.id == cartId).ToListAsync();
+            _dbSet.RemoveRange(cartItems);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ClearAllCart(string accountId)
+        {
+            var cart = await _dbSet.Where(c => c.accountId == accountId).ToListAsync();
+            _dbSet.RemoveRange(cart);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repository/InterfaceRepository/ICartRepository.cs
@@ -0,0 +1,10 @@
+﻿using ShopPC.Models;
+namespace ShopPC.Repository.InterfaceRepository
+{
+    public interface ICartRepository: IGenericRepository<Cart>
+    {
+        Task<Cart?> GetCartByUserIdAsync(string userId);
+        Task ClearCartAsync(string cartId);
+        Task ClearAllCartAsync(string accountId);
+    }
+}
