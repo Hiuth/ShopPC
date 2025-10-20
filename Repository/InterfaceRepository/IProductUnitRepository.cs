a76122af26cc371671902870cb78ea7f73c73d52
Hiuth <Hiuth2004@gmail.com>
2025-10-20 16:44:24 +0700

    Add ProductUnitRepository and related interface
    Added the `IProductUnitRepository` interface to define a method for retrieving `ProductUnit` entities by `productId`. Implemented this interface in the `ProductUnitRepository` class, which extends `GenericReopository<ProductUnit>` and uses Entity Framework to query the database. Updated necessary `using` directives to include required namespaces.

---
+++ Repository/InterfaceRepository/IProductUnitRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShopPC.Models;
+using System.Linq.Expressions;
+namespace ShopPC.Repository.InterfaceRepository
+{
+    public interface IProductUnitRepository: IGenericRepository<ProductUnit>
+    {
+        Task<List<ProductUnit>> GetProductUnitsByProductIdAsync(string productId);
+    }
+}
